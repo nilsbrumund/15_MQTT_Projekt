ef68f3cef77865822eaa4a542652b7268cc9010e
nilsbrumun <nils.brumund@ewetel.net>
2026-04-09 13:44:48 +0200

    Update Displaytest_farben.py

--- Displaytest_farben.py
+++ Displaytest_farben.py
@@ -3,10 +3,7 @@ import st7789py as st7789
 import time
 import vga2_16x32 as font
 
-# ── Init ───────────────────────────────────────────────────
-bl = Pin(35, Pin.OUT)
-bl.value(1)
-
+# ---Display Init-----------
 spi = SPI(2, baudrate=20000000, polarity=0, phase=0,
           sck=Pin(39), mosi=Pin(40))
 custom_rotations = (
@@ -14,35 +11,28 @@ custom_rotations = (
         (0x60, 320, 170, 0, 35, False),
         (0xC0, 170, 320, 35, 0, False),
         (0xA0, 320, 170, 0, 35, False),
-    )
+        )
 
 tft = st7789.ST7789(
     spi, 170, 320,
-    reset=Pin(37,  Pin.OUT),
-    cs=Pin(35,     Pin.OUT),
-    dc=Pin(36,     Pin.OUT),
+    reset=Pin(37, Pin.OUT),
+    cs=Pin(35, Pin.OUT),
+    dc=Pin(36, Pin.OUT),
     backlight=Pin(38, Pin.OUT),
     custom_rotations=custom_rotations,
     rotation=1,
     color_order=st7789.BGR,
 )
 tft.inversion_mode(True)
-
-# ── Test 1: Vollbild-Farben
-text1 = "Nils-Empfaenger"
-text2 = "Babysteps"
-
-char_h = 32
-
 tft.fill(st7789.BLACK)
 
-# ── Zeile 1: normal oben ───────────────────────────────────
-tft.rotation(1)
-x1 = (320 - len(text1) * 16) // 2
-tft.text(font, text1, x1, 0, st7789.WHITE, st7789.BLACK)
+# --------Variablen definieren--------------
+header = "Nils-Empfaenger"
+x1 = 0; y1 = 0; y2 = 32; y3 = 64; y4 = 96; y5 = 128
+temp=27.91
+humi=80
 
-# ── Zeile 2: 180° gedreht unten ───────────────────────────
-tft.rotation(3)
-# Bei 180° ist y=0 unten – also text2 erscheint unterhalb von text1
-x2 = (320 - len(text2) * 16) // 2
-tft.text(font, text2, x2, 0, st7789.YELLOW, st7789.BLACK)
+
+tft.text(font, header,  x1, y1, st7789.WHITE,  st7789.BLACK)
+tft.text(font, f"Temp: {temp}C",   x1, y2, st7789.YELLOW, st7789.BLACK)
+tft.text(font, f"Humi: {humi}%",   x1, y3, st7789.CYAN,   st7789.BLACK)
